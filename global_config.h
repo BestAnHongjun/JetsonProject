adb72296c4c27b6efc27887f60a96cea380916a1
nx <an.hongjun@foxmail.com>
2023-02-13 23:56:33 +0800

    update global config

--- global_config.h
+++ global_config.h
@@ -4,14 +4,15 @@
 /*********** Edit Here ***********/
 
 
-
-/*********** For Debug ***********/
-#define TIMER_SWITCH    1
-#define UDP_SWITCH      0
+/********** Module Set ***********/
+// UDP
 #define UDP_PACK_SIZE   512
 #define UDP_QUE_SIZE    1000
 #define UDP_TIMEOUT     3
 
+/*********** For Debug ***********/
+#define TIMER_SWITCH    1
+#define UDP_SWITCH      0
 
 #define ERR_INFO __FILE__ << ":" << __LINE__
 
