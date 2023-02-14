024dbe94cab94222aa74e6ef99bd8037c267ea3d
nx <an.hongjun@foxmail.com>
2023-02-14 17:06:07 +0800

    update

--- module/image_trans/image_receiver_example.cpp
+++ module/image_trans/image_receiver_example.cpp
@@ -1,15 +1,21 @@
 #include "image_trans.h"
+#include "opencv.h"
 #include <cstdio>
 
+const char* win = "View";
 
 void call_back(cv::Mat mat, double delay, double lost_rate)
 {
     printf("Recv %ld bytes, delay:%.5lf ms, lost_rate: %.2lf% \n", mat.rows * mat.step[0], delay, lost_rate * 100);
+    cv::imshow(win, mat);
+    cv::waitKey(10);
 }
 
 int main()
 {
+    cv::namedWindow(win, cv::WINDOW_AUTOSIZE);
     IMGReceiver img_receiver(8888, "192.168.137.97", 8889, call_back);
+    
     while (1);
     return 0;
 }
